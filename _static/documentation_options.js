ebe04dd3910e991d01b50107b065b2d67f40a0e7
Ralf Hemmecke <ralf@hemmecke.org>
2021-04-05 12:32:58 +0200

    FriCAS 121d1e9ed481fc7a41485d94b93a3f2798bef60a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'd2d82c3317ba73242ae3f50891f65670f3dd3f1e',
+    VERSION: '121d1e9ed481fc7a41485d94b93a3f2798bef60a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
