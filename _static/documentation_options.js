d15be82bc9591f591fd267274f8ba74b58434771
Ralf Hemmecke <ralf@hemmecke.org>
2023-07-02 02:36:35 +0200

    FriCAS 1.3.9pre 02-Jul-2023

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'wip/fricas-sbcl-hunchentoot 2023-03-26',
+    VERSION: 'a5a8df68bd4ab2aab432be7daf6480f2f3fef455',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
